e22aa83dd4f3651ec832856ced4e7d5d88bb463d
francosalvucci14 <francosalvucci14@gmail.com>
2023-12-18 16:13:03 +0100

    Update 00-Init.sql

--- esempi/00-Init.sql
+++ esempi/00-Init.sql
@@ -1,6 +1,6 @@
+use ProgettoVroomA;
 
 CREATE TABLE Personale(
-
 	ID int not null ,
 	Nome varchar(50) not null,
 	Cognome varchar(50) not null,
@@ -10,25 +10,32 @@ CREATE TABLE Personale(
 	PRIMARY KEY (ID)
 );
 
+CREATE TABLE AddettiMarketing (
+	ID_Addetto int not null,
+	Ruolo varchar(50),
+	PRIMARY KEY (ID_Addetto),
+	FOREIGN KEY (ID_Addetto) REFERENCES Personale(ID)
+);
+
 CREATE TABLE Patente (
 	NumeroPatente int not null,
 	DDS date not null,
 	Categoria varchar(50),
 	PRIMARY KEY (NumeroPatente)
 );
-
 CREATE TABLE Offerte (
 	ID_Offerta int not null ,
 	PromoCode int not null,
 	InfoOfferta varchar(50) not null,
-	PRIMARY KEY (ID_Offerta)
+	ID_Addetto int not null,
+	PRIMARY KEY (ID_Offerta),
+	FOREIGN KEY (ID_Addetto) REFERENCES AddettiMarketing(ID_Addetto)
 );
-
 CREATE TABLE Manutentori (
 	ID_Manutentore int not null ,
 	Qualifica varchar(50) not null,
 	PRIMARY KEY (ID_Manutentore),
-	FOREIGN KEY (ID_Manutentore) REFERENCES Personale (ID)
+	FOREIGN KEY (ID_Manutentore) REFERENCES Personale (ID) 
 );
 
 CREATE TABLE Assicurazione (
@@ -37,8 +44,7 @@ CREATE TABLE Assicurazione (
 	Tipo varchar(50) not null,
 	PRIMARY KEY (ID_Assicurazione)
 );
-
-CREATE TABLE Veicoli (
+ CREATE TABLE Veicoli (
 	Targa varchar(50) not null,
 	Marca varchar(50) not null,
 	Modello varchar(50) not null,
@@ -47,14 +53,12 @@ CREATE TABLE Veicoli (
 	PRIMARY KEY (Targa),
 	FOREIGN KEY (Assicurazione) REFERENCES Assicurazione(ID_Assicurazione)
 );
-
 CREATE TABLE Turni (
 	ID_Turno int not null ,
 	OrarioInizio int not null,
 	OrarioFine int not null,
 	PRIMARY KEY (ID_Turno)
 );
-
 CREATE TABLE Autisti (
 	ID_Autista int not null ,
 	NumeroPatente int not null,
@@ -62,7 +66,7 @@ CREATE TABLE Autisti (
 	Targa varchar(50) not null,
 	Stipendio int not null,
 	PRIMARY KEY (ID_Autista),
-	FOREIGN KEY (ID_Autista) REFERENCES Personale (ID),
+	FOREIGN KEY (ID_Autista) REFERENCES Personale (ID), 
 	FOREIGN KEY (NumeroPatente) REFERENCES Patente(NumeroPatente),
 	FOREIGN KEY (Turno) REFERENCES Turni(ID_Turno),
 	FOREIGN KEY (Targa) REFERENCES Veicoli(Targa)
@@ -78,6 +82,7 @@ CREATE TABLE Utenti (
 	Abbonamento varchar(50) not null,
 	PRIMARY KEY (ID_Utente),
 	FOREIGN KEY (ID_Offerta) REFERENCES Offerte (ID_Offerta)
+
 );
 
 CREATE TABLE RichiestePrenotazioni (
@@ -93,15 +98,6 @@ CREATE TABLE RichiestePrenotazioni (
 	FOREIGN KEY (ID_Autista) REFERENCES Autisti(ID_Autista)
 );
 
-CREATE TABLE Feedback (
-	ID_Feedback int not null ,
-	Stelle int not null,
-	Commento varchar(255) not null,
-	Data date not null,
-	ID_TrattaCompletata int not null,
-	PRIMARY KEY (ID_Feedback),
-	FOREIGN KEY (ID_TrattaCompletata) REFERENCES TratteCompletate (ID_TrattaC)
-);
 
 CREATE TABLE Carta (
 	NumeroCarta varchar(50) not null,
@@ -121,11 +117,21 @@ CREATE TABLE TratteCompletate (
 	FOREIGN KEY (NumeroCarta) REFERENCES Carta (NumeroCarta)
 );
 
+CREATE TABLE Feedback (
+	ID_Feedback int not null ,
+	Stelle int not null,
+	Commento varchar(255) not null,
+	Data date not null,
+	ID_TrattaCompletata int not null,
+	PRIMARY KEY (ID_Feedback),
+	FOREIGN KEY (ID_TrattaCompletata) REFERENCES TratteCompletate (ID_TrattaC)
+);
+
 CREATE TABLE TratteRifiutate (
 	ID_TrattaR int not null ,
 	Motivazione varchar(255) not null,
 	PRIMARY KEY (ID_TrattaR),
-	FOREIGN KEY (ID_TrattaR) REFERENCES RichiestePrenotazioni (ID_Richiesta)
+	FOREIGN KEY (ID_TrattaR) REFERENCES RichiestePrenotazioni (ID_Richiesta)	
 );
 
 CREATE TABLE ContattaPerGuasto (
