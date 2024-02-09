be33f936f2c8191b9f4014ae60d1f0e1f7b9a984
francosalvucci14 <francosalvucci14@gmail.com>
2024-02-09 14:25:31 +0100

    Update init.sql & queryBuilder

--- esempi/00-Init.sql
+++ esempi/00-Init.sql
@@ -1,76 +1,60 @@
-use ProgettoVroomA;
-
-CREATE TABLE Personale(
-	ID int not null ,
-	Nome varchar(50) not null,
-	Cognome varchar(50) not null,
-	DDN date not null,
-	NumeroDiTelefono varchar(50) not null,
-	Email varchar(255),
-	PRIMARY KEY (ID)
-);
-
-CREATE TABLE AddettiMarketing (
-	ID_Addetto int not null,
-	Ruolo varchar(50),
-	PRIMARY KEY (ID_Addetto),
-	FOREIGN KEY (ID_Addetto) REFERENCES Personale(ID)
-);
+use VroomA;
 
 CREATE TABLE Patente (
-	NumeroPatente varchar(50) not null,
+	NumeroPatente int not null,
 	DDS date not null,
 	Categoria varchar(50),
 	PRIMARY KEY (NumeroPatente)
 );
-CREATE TABLE Offerte (
-	ID_Offerta int not null ,
-	PromoCode int not null,
-	InfoOfferta varchar(50) not null,
-	ID_Addetto int not null,
-	PRIMARY KEY (ID_Offerta),
-	FOREIGN KEY (ID_Addetto) REFERENCES AddettiMarketing(ID_Addetto)
-);
 CREATE TABLE Manutentori (
 	ID_Manutentore int not null ,
+	Nome varchar(25) not null,
+	Cognome varchar(25) not null,
+	Email varchar(50) not null,
+	DDN date not null,
+	NumeroTelefono varchar(25) not null,
 	Qualifica varchar(50) not null,
 	PRIMARY KEY (ID_Manutentore),
-	FOREIGN KEY (ID_Manutentore) REFERENCES Personale (ID) 
 );
-
-CREATE TABLE Assicurazione (
-	ID_Assicurazione int not null,
-	DataScadenza date not null,
-	Tipo varchar(50) not null,
-	PRIMARY KEY (ID_Assicurazione)
-);
- CREATE TABLE Veicoli (
+CREATE TABLE Veicoli (
 	Targa varchar(50) not null,
 	Marca varchar(50) not null,
 	Modello varchar(50) not null,
 	PostiDisponibili int not null,
-	Assicurazione int not null,
-	PRIMARY KEY (Targa),
-	FOREIGN KEY (Assicurazione) REFERENCES Assicurazione(ID_Assicurazione)
-);
-CREATE TABLE Turni (
-	ID_Turno int not null ,
-	OrarioInizio int not null,
-	OrarioFine int not null,
-	PRIMARY KEY (ID_Turno)
+	Matricola int not null,
+	PRIMARY KEY (Targa)
 );
 CREATE TABLE Autisti (
-	ID_Autista int not null ,
-	NumeroPatente varchar(50) not null,
-	Turno int not null,
+	Martricola int not null ,
+	Nome varchar(25) not null,
+	Cognome varchar(25) not null,
+	Email varchar(50) not null,
+	DDN date not null,
+	NumeroTelefono varchar(25) not null,
+	NumeroPatente int not null,
 	Targa varchar(50) not null,
 	Stipendio int not null,
-	PRIMARY KEY (ID_Autista),
-	FOREIGN KEY (ID_Autista) REFERENCES Personale (ID), 
+	PRIMARY KEY (Matricola),
 	FOREIGN KEY (NumeroPatente) REFERENCES Patente(NumeroPatente),
-	FOREIGN KEY (Turno) REFERENCES Turni(ID_Turno),
 	FOREIGN KEY (Targa) REFERENCES Veicoli(Targa)
 );
+
+CREATE TABLE Assicurazione (
+	Numero int not null,
+	DDS date not null,
+	Tipo varchar(50) not null,
+	Stato varchar(25) not null,
+	Targa varchar(50) not null,
+	PRIMARY KEY (ID_Assicurazione),
+	FOREIGN KEY (Targa) REFERENCES Veicoli(Targa)
+);
+
+CREATE TABLE Turni (
+	OrarioInizio int not null,
+	OrarioFine int not null,
+	PRIMARY KEY (OrarioInizio,OrarioFine)
+);
+
 
 CREATE TABLE Utenti (
 	ID_Utente int not null ,
@@ -78,27 +62,36 @@ CREATE TABLE Utenti (
 	Cognome varchar(50) not null,
 	Email varchar(255) not null,
 	Password varchar(255) not null,
-	ID_Offerta int not null,
-	Abbonamento varchar(50) not null,
-	PRIMARY KEY (ID_Utente),
-	FOREIGN KEY (ID_Offerta) REFERENCES Offerte (ID_Offerta)
-
+	PRIMARY KEY (ID_Utente)
 );
 
 CREATE TABLE RichiestePrenotazioni (
-	ID_Richiesta int not null ,
-	PuntoDiRaccolta varchar(50) not null,
-	PuntoDiRilascio varchar(50) not null,
-	DataRichiesta date not null,
-	OrarioRichiesta varchar(50) not null,
-	NumeroPasseggeri int not null,
 	ID_Utente int not null,
-	ID_Autista int not null,
-	PRIMARY KEY (ID_Richiesta),
+	Partenza varchar(50) not null,
+	Arrivo varchar(50) not null,
+	DataRichiesta date not null,
+	OrarioRichiesta varchar(25) not null,
+	NumeroPasseggeri int not null,
+	PRIMARY KEY (ID_Utente,Partenza,Arrivo,DataRichiesta,OrarioRichiesta),
 	FOREIGN KEY (ID_Utente) REFERENCES Utenti(ID_Utente),
-	FOREIGN KEY (ID_Autista) REFERENCES Autisti(ID_Autista)
 );
 
+
+CREATE TABLE Feedback (
+	ID_Feedback int not null ,
+	StelleUtente int not null,
+	CommentoUtente varchar(255) not null,
+	StelleAutista int not null,
+	CommentoAutista varchar(255) not null,
+	ID_Utente int not null,
+	Partenza varchar(50) not null,
+	Arrivo varchar(50) not null,
+	DataRichiesta date not null,
+	OrarioRichiesta varchar(25) not null,
+	PRIMARY KEY (ID_Feedback),
+	FOREIGN KEY (ID_Utente,Partenza,Arrivo,DataRichiesta,OrarioRichiesta) 
+	REFERENCES RichiestePrenotazioni(ID_Utente,Partenza,Arrivo,DataRichiesta,OrarioRichiesta)
+);
 
 CREATE TABLE Carta (
 	NumeroCarta varchar(50) not null,
@@ -110,39 +103,52 @@ CREATE TABLE Carta (
 );
 
 CREATE TABLE TratteCompletate (
-	ID_TrattaC int not null ,
+	ID_Utente int not null,
+	Partenza varchar(50) not null,
+	Arrivo varchar(50) not null,
+	DataRichiesta date not null,
+	OrarioRichiesta varchar(25) not null,
 	Costo int not null,
-	NumeroCarta varchar(50) not null,
-	PRIMARY KEY (ID_TrattaC),
-	FOREIGN KEY (ID_TrattaC) REFERENCES RichiestePrenotazioni (ID_Richiesta),
-	FOREIGN KEY (NumeroCarta) REFERENCES Carta (NumeroCarta)
-);
-
-CREATE TABLE Feedback (
-	ID_Feedback int not null ,
-	StelleUtente int not null,
-	CommentoUtente varchar(255) not null,
-	StelleAutista int not null,
-	CommentoAutista varchar(255) not null,
-	Data date not null,
-	ID_TrattaCompletata int not null,
-	PRIMARY KEY (ID_Feedback),
-	FOREIGN KEY (ID_TrattaCompletata) REFERENCES TratteCompletate (ID_TrattaC)
+	MetodoDiPagamento varchar(50) not null,
+	DataPagamento date not null,
+	OraPagamento date not null,
+	Autista int not null,
+	PRIMARY KEY (ID_Utente,Partenza,Arrivo,DataRichiesta,OrarioRichiesta),
+	FOREIGN KEY (ID_Utente,Partenza,Arrivo,DataRichiesta,OrarioRichiesta) 
+	REFERENCES RichiestePrenotazioni(ID_Utente,Partenza,Arrivo,DataRichiesta,OrarioRichiesta),
+	FOREIGN KEY	(Autista) REFERENCES Autisti(Matricola)
 );
 
 CREATE TABLE TratteRifiutate (
-	ID_TrattaR int not null ,
+	ID_Utente int not null,
+	Partenza varchar(50) not null,
+	Arrivo varchar(50) not null,
+	DataRichiesta date not null,
+	OrarioRichiesta varchar(25) not null,
 	Motivazione varchar(255) not null,
-	PRIMARY KEY (ID_TrattaR),
-	FOREIGN KEY (ID_TrattaR) REFERENCES RichiestePrenotazioni (ID_Richiesta)	
+	Autista int not null,
+	PRIMARY KEY (ID_Utente,Partenza,Arrivo,DataRichiesta,OrarioRichiesta),
+	FOREIGN KEY (ID_Utente,Partenza,Arrivo,DataRichiesta,OrarioRichiesta) 
+	REFERENCES RichiestePrenotazioni(ID_Utente,Partenza,Arrivo,DataRichiesta,OrarioRichiesta),
+	FOREIGN KEY	(Autista) REFERENCES Autisti(Matricola)
 );
 
 CREATE TABLE ContattaPerGuasto (
 	ID_Manutentore int not null,
-	ID_Autista int not null,
+	Matricola int not null,
 	Motivo varchar(255) not null,
+	Data date not null,
 	FOREIGN KEY (ID_Manutentore) REFERENCES Manutentori (ID_Manutentore),
-	FOREIGN KEY (ID_Autista) REFERENCES Autisti (ID_Autista)
+	FOREIGN KEY (Matricola) REFERENCES Autisti (Matricola)
 );
 
-
+CREATE TABLE TabellaOrarioLavorativo(
+	Matricola int not null,
+	OrarioInizio int not null,
+	OrarioFine int not null,
+	Data date not null,
+	PRIMARY KEY(Matricola,OrarioInizio, OrarioFine),
+	FOREIGN KEY	(Autista) REFERENCES Autisti(Matricola),
+	FOREIGN KEY	(OrarioInizio) REFERENCES Turni(OrarioInizio),
+	FOREIGN KEY	(OrarioFine) REFERENCES Turni(OrarioFine),
+);
